f892f7c309e947610576ee53ff109af39cc8e456
fabricematrat <fabricematrat@gmail.com>
2013-07-14 17:58:55 +0200

    simplify

--- web-app/js/threecircles/view/checkin-view.js
+++ web-app/js/threecircles/view/checkin-view.js
@@ -91,6 +91,7 @@ threecircles.view.checkinview = function (model, elements) {
     // user interface actions
     that.elements.list.on('pageinit', function (e) {
         that.listButtonClicked.notify();
+        geolocationBackground.showMapBackground('map_canvas', {}) ;
     });
 
     that.elements.list.on('pageshow', function (e) {
