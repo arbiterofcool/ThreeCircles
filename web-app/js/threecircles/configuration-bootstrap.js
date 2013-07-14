c5a7e9edc7a028b8fc0755129d46cb8f215f4d6e
fabricematrat <fabricematrat@gmail.com>
2013-07-14 12:53:10 +0200

    simplify

--- web-app/js/threecircles/configuration-bootstrap.js
+++ web-app/js/threecircles/configuration-bootstrap.js
@@ -3,6 +3,8 @@ var threecircles = threecircles || {};
 threecircles.loadConfiguration = (function () {
     threecircles.configuration = {
         baseURL: "http://localhost:8080/ThreeCircles/",
+        //Uncomment for Android emulator localhost
+        //baseURL: "http://10.0.2.2:8080/ThreeCircles/",
         //Uncomment before pushing to cloudfoundry
         //baseURL: "http://ThreeCircles.cloudfoundry.com/",
         namespace: "threecircles",
