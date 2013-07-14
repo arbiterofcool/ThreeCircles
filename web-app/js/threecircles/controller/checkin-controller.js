ab1354d374146b0c99adf28c8e5965c19f548ba3
fabricematrat <fabricematrat@gmail.com>
2013-07-14 13:21:25 +0200

    simplify

--- web-app/js/threecircles/controller/checkin-controller.js
+++ web-app/js/threecircles/controller/checkin-controller.js
@@ -19,7 +19,7 @@ threecircles.controller.checkincontroller = function (feed, model, view, cfg) {
         $.ajax({
             cache: false,
             type: "POST",
-            async: false,
+            async: true,
             data: item,
             dataType: "json",
             url: url,
