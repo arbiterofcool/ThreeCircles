9401cc4eb16c6795397c3251cb7901ada579cf28
fabricematrat <fabricematrat@gmail.com>
2013-07-14 12:03:01 +0200

    simplify

--- web-app/js/threecircles/controller/checkin-controller.js
+++ web-app/js/threecircles/controller/checkin-controller.js
@@ -2,7 +2,7 @@ var threecircles = threecircles || {};
 threecircles.controller = threecircles.controller || {};
 
 threecircles.controller.checkincontroller = function (feed, model, view, cfg) {
-    var that = grails.mobile.mvc.controller(feed, model, view), cfg;
+    var that = grails.mobile.mvc.controller(feed, model, view, cfg);
 
     //Place here your custom event
 //    view.somethingButtonClicked.attach(function (item, context) {
--- web-app/js/threecircles/view/checkin-view.js
+++ web-app/js/threecircles/view/checkin-view.js
@@ -4,7 +4,12 @@ threecircles.view = threecircles.view || {};
 threecircles.view.checkinview = function (model, elements) {
 
     var that = grails.mobile.mvc.view(model, elements);
-    var timeline = threecirclesconfess.view.timeline();
+    var timeline = threecircles.view.timeline();
+
+    that.init = function () {
+        that.listButtonClicked.notify();
+    };
+
 
     // Register events
     that.model.listedItems.attach(function (data) {
@@ -65,7 +70,7 @@ threecircles.view.checkinview = function (model, elements) {
             if (!data.item.NOTIFIED) {
                 $.mobile.changePage($('#section-list-checkin'));
             }
-		}
+        }
     });
 
     that.model.updatedItem.attach(function (data, event) {
@@ -143,72 +148,42 @@ threecircles.view.checkinview = function (model, elements) {
         event.stopPropagation();
         $('#form-update-checkin').validationEngine('hide');
         $('#form-update-checkin').validationEngine({promptPosition: 'bottomLeft'});
-        that.editButtonClicked.notify();
         createElement();
+        that.editButtonClicked.notify();
     });
 
     var show = function(dataId, event) {
         event.stopPropagation();
         $('#form-update-checkin').validationEngine('hide');
         $('#form-update-checkin').validationEngine({promptPosition: 'bottomLeft'});
-        that.editButtonClicked.notify();
         showElement(dataId);
+        that.editButtonClicked.notify(function () {
+            showDependentElement(dataId);
+        });
     };
 
     var createElement = function () {
         resetForm('form-update-checkin');
         $.mobile.changePage($('#section-show-checkin'));
         $('#delete-checkin').css('display', 'none');
+        that.editButtonClicked.notify(function () {
+        });
     };
 
     var showElement = function (id) {
         resetForm('form-update-checkin');
+        showDependentElement(id);
         var element = that.model.items[id];
-        var value = element['owner.id'];
-        if (!value) {
-            value = element['owner'];
-        }
-        if (!value || (value === Object(value))) {
-           value = element.owner.id;
-        }
-        $('select[data-gorm-relation="many-to-one"][name="owner"]').val(value).trigger("change");
-        
-        var value = element['place.id'];
-        if (!value) {
-            value = element['place'];
-        }
-        if (!value || (value === Object(value))) {
-           value = element.place.id;
-        }
-        $('select[data-gorm-relation="many-to-one"][name="place"]').val(value).trigger("change");
-        
-        var commentsSelected = element.comments;
-        $.each(commentsSelected, function (key, value) {
-            var selector;
-            if (value === Object(value)) {
-              selector= '#checkbox-comments-' + value.id;
-            } else {
-              selector= '#checkbox-comments-' + value;
-            }
-            $(selector).attr('checked','checked').checkboxradio('refresh');
-        });
-        var friendsSelected = element.friends;
-        $.each(friendsSelected, function (key, value) {
-            var selector;
-            if (value === Object(value)) {
-              selector= '#checkbox-friends-' + value.id;
-            } else {
-              selector= '#checkbox-friends-' + value;
-            }
-            $(selector).attr('checked','checked').checkboxradio('refresh');
-        });
         $.each(element, function (name, value) {
             var input = $('#input-checkin-' + name);
             if (input.attr('type') != 'file') {
                 input.val(value);
             } else {
-                var img = grails.mobile.camera.encode(value);
-                input.parent().css('background-image', 'url("' + img + '")');
+                if (value) {
+                    var img = grails.mobile.camera.encode(value);
+                    input.parent().css('background-image', 'url("' + img + '")');
+                    input.attr('data-value', img);
+                }
             }
             if (input.attr('data-type') == 'date') {
                 input.scroller('setDate', (value === '') ? '' : new Date(value), true);
@@ -216,6 +191,48 @@ threecircles.view.checkinview = function (model, elements) {
         });
         $('#delete-checkin').show();
         $.mobile.changePage($('#section-show-checkin'));
+    };
+
+    var showDependentElement = function (id) {
+        var element = that.model.items[id];
+        var value = element['owner.id'];
+        if (!value) {
+            value = element['owner'];
+        }
+        if (!value || (value === Object(value))) {
+            value = element.owner.id;
+        }
+        $('select[data-gorm-relation="many-to-one"][name="owner"]').val(value).trigger("change");
+
+        var value = element['place.id'];
+        if (!value) {
+            value = element['place'];
+        }
+        if (!value || (value === Object(value))) {
+            value = element.place.id;
+        }
+        $('select[data-gorm-relation="many-to-one"][name="place"]').val(value).trigger("change");
+
+        var commentsSelected = element.comments;
+        $.each(commentsSelected, function (key, value) {
+            var selector;
+            if (value === Object(value)) {
+                selector= '#checkbox-comments-' + value.id;
+            } else {
+                selector= '#checkbox-comments-' + value;
+            }
+            $(selector).attr('checked','checked').checkboxradio('refresh');
+        });
+        var friendsSelected = element.friends;
+        $.each(friendsSelected, function (key, value) {
+            var selector;
+            if (value === Object(value)) {
+                selector= '#checkbox-friends-' + value.id;
+            } else {
+                selector= '#checkbox-friends-' + value;
+            }
+            $(selector).attr('checked','checked').checkboxradio('refresh');
+        });
     };
 
     var resetForm = function (form) {
@@ -243,7 +260,7 @@ threecircles.view.checkinview = function (model, elements) {
             });
         }
     };
-    
+
 
     var refreshSelectDropDown = function (select, newOptions) {
         var options = null;
@@ -259,6 +276,7 @@ threecircles.view.checkinview = function (model, elements) {
             });
             select.val(options[0]);
         }
+        select.selectmenu("refresh");
     };
 
     var renderDependentList = function (dependentName, items) {
@@ -290,7 +308,7 @@ threecircles.view.checkinview = function (model, elements) {
         });
         refreshMultiChoices(oneToMany, dependentName, options);
     };
-    
+
     var createListItem = function (element) {
         var li, a = $('<a>');
         a.attr({
@@ -302,7 +320,7 @@ threecircles.view.checkinview = function (model, elements) {
         a.on('vclick', function(event) {
             show(element.id, event);
         });
-        
+
         if (element.offlineStatus === 'NOT-SYNC') {
             li =  $('<li>').attr({'data-theme': 'e'});
             li.append(a);
