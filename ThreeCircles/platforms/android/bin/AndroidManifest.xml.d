ca29150ef7c1452b9bedb62a1a42799d503399bd
corinnekrych <corinnekrych@gmail.com>
2013-07-17 23:22:21 +0200

    remove hardcoded value in cordova

--- ThreeCircles/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/corinne/workspace/gr8confus/ThreeCircles/ThreeCircles/platforms/android/bin/AndroidManifest.xml : \
-/Users/corinne/workspace/gr8confus/ThreeCircles/ThreeCircles/platforms/android/AndroidManifest.xml \
