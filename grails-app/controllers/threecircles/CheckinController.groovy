b463e86db6765566c0cb46a47495b4e34f6cfde2
fabricematrat <fabricematrat@gmail.com>
2013-07-18 01:44:32 +0200

    simplify

--- grails-app/controllers/threecircles/CheckinController.groovy
+++ grails-app/controllers/threecircles/CheckinController.groovy
@@ -1,7 +1,9 @@
 package threecircles
 
 
-import grails.converters.deep.JSON
+
+import grails.converters.JSON
+import groovy.json.JsonBuilder
 import org.grails.datastore.mapping.validation.ValidationErrors
 import org.springframework.dao.DataIntegrityViolationException
 import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
@@ -47,7 +49,12 @@ class CheckinController {
       }
       
       def asJson = checkinInstance as JSON
-      event topic:"save-checkin", data: asJson.toString()
+      def builder = new JsonBuilder()
+      builder {
+        userIdNotification  params.userIdNotification
+        instance  asJson.toString()
+      }
+      event topic:"save-checkin", data: builder.toString()
       render checkinInstance as JSON
     }
     
@@ -110,7 +117,12 @@ class CheckinController {
       }
       
       def asJson = checkinInstance as JSON
-      event topic:"update-checkin", data: asJson.toString()
+      def builder = new JsonBuilder()
+      builder {
+          userIdNotification  params.userIdNotification
+          instance  asJson.toString()
+      }
+      event topic:"update-checkin", data: builder.toString()
       render checkinInstance as JSON
     }
 
@@ -139,7 +151,13 @@ class CheckinController {
         return
       }
       
-      event topic:"delete-checkin", data: checkinInstance
+      def asJson = checkinInstance as JSON
+      def builder = new JsonBuilder()
+      builder {
+          userIdNotification  params.userIdNotification
+          instance  asJson.toString()
+      }
+      event topic:"delete-checkin", data: builder.toString()
       render checkinInstance as JSON
     }
     
